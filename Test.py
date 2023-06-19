0e7618913ffce3a21d6afd635af2cab21a717df4
Delta1 <andita.rossoneri1911@gmail.com>
2023-06-19 22:09:17 +0700

    first commit

---
+++ Test.py
@@ -0,0 +1,18 @@
+import time
+start_time = time.time (   )
+print ("bararade anjay")
+print ("kamana atuhhhh")
+
+# bacoooot, comment
+
+a=10 #ini adalah variable
+print (a)
+"""ini catatan andi, comment multiline"""
+
+#kita bisa mengcompile python ke
+# yg namanya bytecode
+#cara mengcompile, buka terminal dan tuliskan
+#phyton -m py_compile Test.py
+for i in range (1,1000):
+    a=10
+print (time.time() - start_time, "detik")
